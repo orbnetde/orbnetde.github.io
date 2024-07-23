4210e23fce27ccbd7849a548bf8b78fcd9a483bc
orbnetde <info@orbnet.de>
2024-07-23 20:15:35 +0700

    chore: website update

--- src/utils/all.ts
+++ src/utils/all.ts
@@ -12,5 +12,13 @@ export const getFormattedDate = (date: string): string =>
 
 
 export const getAnchorFromText = (text: string): string => {
-  return String(text).toLowerCase().replace(/[^a-zA-Z0-9 ]+/g, '').replace(/ /g, '-');
+  return String(text)
+    .toLowerCase()
+    .replace(/\u00fc/g, 'ue')
+    .replace(/\u00e4/g, 'ae')
+    .replace(/\u00f6/g, 'oe')
+    .replace(/\u00df/g, 'ss')
+    .replace(/[^a-z0-9\-. ]+/g, '')
+    .replace(/ /g, '-')
+    .replace(/-{2,}/g, '-');
 };
