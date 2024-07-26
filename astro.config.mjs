cbe5d67610cc01988882015d994991c22e22b1f0
orbnetde <info@orbnet.de>
2024-07-26 19:24:41 +0700

    chore: website update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import compress from 'astro-compress';
 import compressor from 'astro-compressor';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import jopSoftwareCookieConsent from '@jop-software/astro-cookieconsent';
 import icon from 'astro-icon';
 import storyblok from '@storyblok/astro';
 
@@ -35,6 +36,51 @@ export default defineConfig({
   integrations: [
     tailwind({applyBaseStyles: false}),
     sitemap(),
+    import.meta.env.PROD && jopSoftwareCookieConsent({
+      categories: {
+        analytics: {},
+      },
+      language: {
+        default: 'de',
+        translations: {
+          de: {
+            consentModal: {
+              title: 'Wir nutzen Cookies',
+              description: 'Wir verwenden Cookies und andere Technologien auf unserer Webseite. Einige von ihnen sind essenziell, während andere uns helfen, diese Webseite und Ihre Erfahrung zu verbessern. Weitere Informationen über die Verwendung Ihrer Daten finden Sie in unserer Datenschutzerklärung.',
+              acceptAllBtn: 'Alle akzeptieren',
+              acceptNecessaryBtn: 'Alle ablehnen',
+              showPreferencesBtn: 'Individuelle Einstellungen',
+            },
+            preferencesModal: {
+              title: 'Cookie Einstellungen bearbeiten',
+              acceptAllBtn: 'Alle akzeptieren',
+              acceptNecessaryBtn: 'Alle ablehnen',
+              savePreferencesBtn: 'Aktuelle Auswahl akzeptieren',
+              closeIconLabel: 'Schließen',
+              sections: [{
+                title: 'Google Analytics',
+                description: 'Cookie von Google für Website-Analysen. Erzeugt statistische Daten darüber, wie der Besucher die Website nutzt.',
+                linkedCategory: 'analytics',
+              }],
+            },
+          },
+        },
+      },
+      guiOptions: {
+        consentModal: {
+          layout: 'bar inline',
+          position: 'bottom',
+          equalWeightButtons: true,
+          flipButtons: false,
+        },
+        preferencesModal: {
+          layout: 'bar wide',
+          position: 'right',
+          equalWeightButtons: true,
+          flipButtons: false,
+        },
+      },
+    }),
     alpinejs(),
     icon({
       include: {
